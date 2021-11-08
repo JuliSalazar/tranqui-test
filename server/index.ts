6ac5c0e1f3aeba655f06c4167be3a3bfda9ed90a
Julieth Salazar <Juli.Salazar18@hotmail.com>
2021-11-08 08:00:36 -0500

    update data txt - back

--- server/index.ts
+++ server/index.ts
@@ -3,39 +3,75 @@ import express from 'express';
 const app = express();
 const port = 3333;
 const bp = require('body-parser');
+var fs = require('fs');
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-var base = [
+type nameType = {
+  name: string,
+  count: number
+};
+
+var base: nameType[] = [
   {
-    "name": "Luis Florez"
+    name: "Luis Florez",
+    count: 0
   },
   {
-    "name": "David Castaño"
+    name: "David Ampudia",
+    count: 0
   }, {
-    "name": "Joel Arias"
+    name: "Joel Arias",
+    count: 0
   }];
+
 app.get('/api', (req, res) => {
   res.json(base);
 });
-app.post('/api/count',(req, res) => {
-  /* let data = {name: req.body.name}; */
-console.log(req.body);
 
+app.post('/api/count', (req, res) => {
+  let dataFromClient: nameType[] = req.body;
+  let updatedData: string = "";
+
+  fs.readFile("server/counter.txt", "utf-8", (err: string, data: string) => {
+    if (err) { console.log(err) }
+    let dataLines = data.split('\r\n');
+    let changed: string = "";
+
+    let newName = false;
+
+    dataFromClient.forEach(element => {
+      let name: string = element.name;
+      let count: number = element.count;
+      console.log(count);
+      dataLines.forEach((line, index) => {
+
+//array filter? why only 6 updates
+        if (line.includes(name) && count.toString() !== line.split(': ')[1]) {
+
+          changed = line.replace(line.split(': ')[1], count.toString());
+          updatedData = dataLines.join('\r\n').replace(line, changed);
+          fs.writeFile('server/counter.txt', updatedData, function (err: string) {
+            if (err) { console.log(err) }
+            console.log("Overwrited data");
+          })
+        } else if (line.split(': ')[0] !== name) {
+          console.log("DIF" + line);
+
+          /* fs.appendFile('server/counter.txt', name+": "+count+'\r\n', function (err: string) {
+            if (err) { console.log(err) }
+            console.log("New Data");
+          })  */
+        }
+      });
+    });
+    /*  if(newName){
+ 
+       
+     } */
+
+  });
 });
-
-/* var fs = require('fs');
-fs.readFile("server/counter.txt", "utf-8", (err: string, data: string) => {
-  if (err) { console.log(err) }
-  console.log(data);
-});
-
-fs.appendFile('server/counter.txt', '\r\nnew data', function (err: string) {
-  if (err) { console.log(err) }
-  console.log("Successfully Written to File.");
-}) */
-
-
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,15 @@
 import { Container, CssBaseline, Paper, Typography, ButtonGroup, Button } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect } from 'react';
-import {  BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
+type nameType = {
+  name: string,
+  count: number
+};
 
 function App() {
-
-  const [initialNames, setInitialNames] = React.useState<any[]>([]);
+  const [initialNames, setInitialNames] = React.useState<nameType[]>([]);
 
   useEffect(() => {
     const loadInitialData = async () => {
@@ -16,53 +19,49 @@ function App() {
       setInitialNames(array);
     };
     loadInitialData();
+     
   }, []);
 
-  const handleClick = () => {
-    initialNames.forEach((prev: any[],i)=>{
-      //dont forget to create type of 
-      console.log()
-    });
-   /*  setInitialNames(prev.forEach(() => {
+  const handleClick = (index: number) => {
 
-    })); */
+    initialNames[index].count = initialNames[index].count + 1;
     const postData = async () => {
-    const response = await fetch(`/api/count`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(initialNames),
-    })
-  return await console.log(response);
+      const response = await fetch(`/api/count`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(initialNames),
+      })
+      return await console.log(response);
+    }
+    postData();
+    
+    console.log("clicked")
   }
-  postData();
-}
 
   return (<>
-  <BrowserRouter>
-    <CssBaseline />
-    <Box bgcolor="#eeeeee" minHeight="100vh" p={4}>
-      <Container maxWidth="sm">
-        <Paper>
-          <Box p={4}>
-            <Typography variant="h4">People to click</Typography>
-            <ButtonGroup
-              orientation="vertical"
-              aria-label="vertical outlined button group"
-            >
+    <BrowserRouter>
+      <CssBaseline />
+      <Box bgcolor="#eeeeee" minHeight="100vh" p={4}>
+        <Container maxWidth="sm">
+          <Paper>
+            <Box p={4}>
+              <Typography variant="h4">People to click</Typography>
+              <ButtonGroup
+                orientation="vertical"
+                aria-label="vertical outlined button group"
+              >
 
-              {initialNames.map(({ name }, index ) => {
-                return (
-                  <Link key={name} to="/api/count">
-                  <Button key={name} onClick={handleClick} type="submit">{name}</Button>
-                  </Link>
-                
-                );
-              })}
-            </ButtonGroup>
-          </Box>
-        </Paper>
-      </Container>
-    </Box>
+                {initialNames.map(({ name }, index) => {
+                  return (
+                    <Button key={index} variant="contained" color="primary" onClick={() => { handleClick(index) }}>{name}</Button>
+
+                  );
+                })}
+              </ButtonGroup>
+            </Box>
+          </Paper>
+        </Container>
+      </Box>
     </BrowserRouter>
   </>);
 }
